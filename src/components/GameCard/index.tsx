c3021be9e232144a8b8efbf23808580af942ca7e
Yuri Witney <yuriwfbarros@gmail.com>
2022-09-17 19:35:56 -0300

    feat: finish lesson four

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -5,9 +5,9 @@ import { THEME } from "../../theme"
 
 export interface GameCardProps {
   id: string
-  name: string
-  ads: string
-  cover: ImageSourcePropType
+  title: string
+  _count: {ads: number}
+  bannerUrl: string
 
 }
 
@@ -20,7 +20,7 @@ export function GameCard({data, ...rest}: Props) {
     <TouchableOpacity style={styles.container} {...rest}>
       <ImageBackground
         style= {styles.cover}
-        source= {data.cover}
+        source= {{uri: data.bannerUrl}}
       >
 
         <LinearGradient
@@ -28,11 +28,11 @@ export function GameCard({data, ...rest}: Props) {
           style={styles.footer}
         >
           <Text style={styles.name}>
-            {data.name}
+            {data.title}
           </Text>
 
           <Text style={styles.ads}>
-            {data.ads} anúncios
+            {data._count.ads} anúncio(s)
           </Text>
         </LinearGradient>
       </ImageBackground>
